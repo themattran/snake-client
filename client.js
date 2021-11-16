a4ad19baa7eea973ab0a9b9e0bc192c4e6aee46f
Matthew Tran <m.tran906@gmail.com>
2021-11-16 02:21:08 +0000

    we use the ES6 object shorthand notation to add the setupInput function to input.js's module.exports object.

--- client.js
+++ client.js
@@ -1,6 +1,42 @@
-const {connect, setupInput} = require('./play'); 
+const net = require("net");
 
-connect(); 
-setupInput(); 
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.0.2.15', // IP address here,
+    port: 50541 // PORT number here,
+  });
 
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log('connection successful'); 
+    //conn.write(`Name: KEV`);
+    
+    setInterval(() => {
+      conn.write(`Move: right`);
+    }, 50)
+
+    setInterval(() => {
+      conn.write(`Move: down`);
+    }, 100)
+
+    setInterval(() => {
+      conn.write(`Move: right`);
+    }, 250)
+
+
+  })
+  
+  conn.on("data", (data) => {
+    console.log('Snek says:', data);
+  })
+
+  return conn;
+};
+
+module.exports = {
+  connect
+}; 
 
--- play.js
+++ play.js
@@ -1,63 +1,10 @@
-const net = require("net");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.2.15', // IP address here,
-    port: 50541 // PORT number here,
-  });
+console.log("Connecting ...");
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+connect();
 
-  conn.on("connect", () => {
-    console.log('connection successful'); 
-    //conn.write(`Name: KEV`);
-    
-    setInterval(() => {
-      conn.write(`Move: right`);
-    }, 50)
-
-    setInterval(() => {
-      conn.write(`Move: down`);
-    }, 100)
-
-    setInterval(() => {
-      conn.write(`Move: right`);
-    }, 250)
+setupInput();
 
 
-  })
-  
-  conn.on("data", (data) => {
-    console.log('Snek says:', data);
-  })
-
-  return conn;
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-
-    if (key === '\u0003') {
-      process.exit(); 
-    }
-
-
-}; 
-
-// console.log("Connecting ...");
-// connect();
-
-module.exports = {
-  connect,
-  setupInput
-}; 
